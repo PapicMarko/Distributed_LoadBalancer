6d3ba74f78c54e58597732b9b28e50463a5c2300
Marko Papic <32815437+PapicMarko@users.noreply.github.com>
2023-11-18 17:04:06 +0100

    Dynamic LoadBalancer - adding, removing servers

--- load_balancer.py
+++ load_balancer.py
@@ -7,8 +7,28 @@ class RoundRobinLoadBalancer:
         server = self.servers[self.current_index]
         self.current_index = (self.current_index + 1) % len(self.servers)
         return server
+    
+class DynamicLoadBalancer:
+    def __init__(self):
+        self.servers = []
+        self.current_index = 0
 
-# Example usage
+    def add_server(self, server):
+        self.servers.append(server)
+
+    def remove_server(self, server):
+        if server in self.servers:
+            self.servers.remove(server)
+
+    def get_next_server(self):
+        if not self.servers:
+            raise ValueError("No servers available in the server pool")
+        
+        server = self.servers[self.current_index]
+        self.current_index = (self.current_index + 1) % len(self.servers)
+        return server
+
+# Example usage (RoundRobin)
 servers = ["Server1", "Server2", "Server3", "Server4", "Server5"]
 load_balancer = RoundRobinLoadBalancer(servers)
 
@@ -16,3 +36,26 @@ load_balancer = RoundRobinLoadBalancer(servers)
 for _ in range(10):
     next_server = load_balancer.get_next_server()
     print(f"Request directed to: {next_server}")
+
+
+# Example usage (DynamicLoadBalancer)
+load_balancer = DynamicLoadBalancer()
+
+# Add servers dynamically
+load_balancer.add_server("Server1")
+load_balancer.add_server("Server2")
+load_balancer.add_server("Server3")
+
+# Get the next server for each request
+for _ in range(10):
+    next_server = load_balancer.get_next_server()
+    print(f"Request directed to: {next_server}")
+
+# Remove a server dynamically
+load_balancer.remove_server("Server2")
+
+# Get the next server after removal
+next_server_after_removal = load_balancer.get_next_server()
+print(f"Request directed to: {next_server_after_removal}")
+
+
